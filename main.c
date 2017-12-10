29c61342df4e2386a03d284d1ccd7e2f2193ee73
Josh Collins <jjcollins2@crimson.ua,edu>
2017-12-09 18:11:58 -0600

    Restructuring for visibility

--- main.c
+++ main.c
@@ -6,109 +6,25 @@
 
 int main() {
 
-
-/*  If you want to see the ugliest debug code ever, look no further!
-
-    int vn = 1, sysid = 0, docid = 0;
-
-    //Record 1
-    RecordAttribute *attribute1 = newRecordAtt("Badge", 1234);
-    Record *record1 = newRecord(++docid, ++sysid, vn);
-    insertAttribute(record1, attribute1);
-    RecordAttribute *attribute2 = newRecordAtt("Phone", 865);
-    insertAttribute(record1, attribute2);
-    RecordAttribute *attribute3 = newRecordAtt("Badge", 9876);
-    insertAttribute(record1, attribute3);
-    printRecord(record1);
-
-    Record *head = record1;
-
-    //Record 2
-    RecordAttribute *attribute11 = newRecordAtt("Area", 341);
-    RecordAttribute *attribute12 = newRecordAtt("Key", 82471);
-    Record *record2 = newRecord(++docid, ++sysid, vn);
-    insertAttribute(record2, attribute11);
-    insertAttribute(record2, attribute12);
-    printf("\n");
-    printRecord(record2);
-
-    //Record 3
-    RecordAttribute *attribute111 = newRecordAtt("Num", 412);
-    RecordAttribute *attribute122 = newRecordAtt("Badge", 444);
-    Record *record3 = newRecord(docid, ++sysid, vn);
-    insertAttribute(record3, attribute111);
-    insertAttribute(record3, attribute122);
-    printf("\n");
-    printRecord(record3);
-
-    //Record 4
-    RecordAttribute *attribute1111 = newRecordAtt("Num", 512);
-    RecordAttribute *attribute1222 = newRecordAtt("Badge", 829);
-    Record *record4 = newRecord(docid, ++sysid, vn);
-    insertAttribute(record4, attribute1111);
-    insertAttribute(record4, attribute1222);
-    printf("\n");
-    printRecord(record4);
-
-    //Record 5
-    RecordAttribute *attribute11111 = newRecordAtt("Num", 112);
-    RecordAttribute *attribute12222 = newRecordAtt("Badge", 812443);
-    Record *record5 = newRecord(1, ++sysid, vn);
-    insertAttribute(record5, attribute11111);
-    insertAttribute(record5, attribute12222);
-    printf("\n");
-    printRecord(record5);
-
-    //Record 6
-    RecordAttribute *attribute111111 = newRecordAtt("Num", 173553);
-    RecordAttribute *attribute122222 = newRecordAtt("Badge", 851443);
-    Record *record6 = newRecord(1, ++sysid, vn);
-    insertAttribute(record6, attribute111111);
-    insertAttribute(record6, attribute122222);
-    printf("\n");
-    printRecord(record6);
-
-    //Record 7
-    RecordAttribute *attribute1111111 = newRecordAtt("Num", 512512);
-    RecordAttribute *attribute1222222 = newRecordAtt("Badge", 1233);
-    Record *record7 = newRecord(++docid, ++sysid, vn);
-    insertAttribute(record7, attribute1111111);
-    insertAttribute(record7, attribute1222222);
-    printf("\n");
-    printRecord(record7);
-
-    //Record 8
-    RecordAttribute *attribute11111111 = newRecordAtt("Num", 123123123);
-    RecordAttribute *attribute12222222 = newRecordAtt("Badge", 123123123);
-    Record *record8 = newRecord(0, ++sysid, vn);
-    insertAttribute(record8, attribute11111111);
-    insertAttribute(record8, attribute12222222);
-    printf("\n");
-    printRecord(record8);
-
-    //Insert the records and make sure to reset the head each update
-
-    head = insertNextRecord(head, record8, NULL, NULL);
-    head = insertNextRecord(head, record2, NULL, NULL);
-    head = insertNextRecord(head, record3, NULL, NULL);
-    head = insertNextRecord(head, record4, NULL, NULL);
-    head = insertNextRecord(head, record5, NULL, NULL);
-    head = insertNextRecord(head, record6, NULL, NULL);
-    head = insertNextRecord(head, record7, NULL, NULL);
-
-
-    printf("\n =========================== \n");
-    printRecordList(head);*/
-
+    /* File handling and error checking */
     FILE *fp = fopen("/Users/joshcollins/cs457/Final/data.txt", "r");
-
-    /* Variables for setting ID's and VN's */
-    int vn = 1, sysID = 1;
 
     if(fp == NULL)  {
         fprintf(stderr, "File could not be open for reading.");
         exit(1);
     }
+
+
+/*
+    This is the code to read in the data, conduct the versioning of multiple records,
+    and create a three deminsional linked list cube structure that acts as a list of
+    unique records, which have a list of versions with the same docID, which has a list
+    of attribute nodes attached to it.
+
+    ================================ R E A D  I N  D A T A =============================
+*/
+    /* Variables for setting ID's and VN's */
+    int vn = 1, sysID = 1;
 
     char ch;
     Record *head;
@@ -159,7 +75,6 @@ int main() {
             if (strcmp(attributeName, "DocID") == 0) {
                 //This is a docID
                 setDocID(nuRecord, attributeNum);
-//                printRecord(nuRecord);
             } else {
                 //This is an attribute
                 RecordAttribute *ra = newRecordAtt(attributeName, attributeNum);
@@ -178,6 +93,9 @@ int main() {
         }
 
     }
+/*
+    ================================ E N D  R E A D=============================
+*/
 
     printRecordList(head);
 
